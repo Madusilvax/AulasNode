1d67174d056bbffc1ef3ddf867b4cd721932e581
Maria Eduarda <ma.eferreira20@gmail.com>
2025-11-11 22:31:10 -0300

    Primeira  aula de node

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+
+const herois = ['mulher Maravilha', 'Superman', 'Batman']
+
+app.get('/herois', function (req, res) {
+    res.send(herois)
+})
+
+app.get('/herois/:id', function (req, res) {
+    const id = req.params.id
+    res.send(herois[id - 1])
+})
+
+app.use(express.json())
+app.post('/herois', function (req, res) {
+    let novoHeroi = req.body.nome
+    herois.push(novoHeroi)
+    res.send("ok")
+})
+
+
+app.listen(3000)
